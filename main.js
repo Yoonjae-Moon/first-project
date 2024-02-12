077ab8ea07dd8a1c822e5d8017350cab7f9bacbc
Yoonjae-Moon <156685601+Yoonjae-Moon@users.noreply.github.com>
2024-02-12 22:51:31 +0900

    main.js

--- main.js
+++ main.js
@@ -13,8 +13,17 @@ let randomNum = 0;
 let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input");
 let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button")
+let chances = 5
+let gameOver = false
+let chanceArea = document.getElementById("chance-area")
+let history=[];
 
 playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
+userInput.addEventListener("focus", function(){
+    userInput.value = "";
+})
 
 function pickRandomNum(){
     randomNum = Math.floor(Math.random() * 100) + 1;
@@ -22,16 +31,52 @@ function pickRandomNum(){
 }
 
 function play(){
-    let userValue = userInput.ariaValueMax;
+    let userValue = userInput.value;
+
+    if (userValue < 1 || userValue > 100){
+        resultArea.textContent = "1과 100 사이의 숫자를 입력해 주세요"
+        return;
+    }
+
+    if (history.includes(userValue)){
+        resultArea.textContent="이미 입력한 숫자입니다 다른 숫자를 입력해 주세요"
+        return;
+    }
+
+    chances--;
+    chanceArea.textContent= `남은 기회: ${chances}번`
+
     if (userValue < randomNum){
         resultArea.textContent = "UP!!!"
     } else if (userValue > randomNum){
         resultArea.textContent = "Down!!!"
     } else {
         resultArea.textContent = "정답입니다!!"
+        gameOver=true
     }
+    history.push(userValue)
+    console.log(history)
 
     
-    console.log(userValue);
+    if (chances < 1){
+        gameOver = true
+    }
+    if (gameOver == true){
+        playButton.disabled = true
+    }
 }
+
+function reset(){
+    //user input 깨끗하게 정리
+    userInput.value = ""
+    chances = 5;
+    //새로운 번호 생성
+    pickRandomNum();
+    
+    resultArea.textContent="결과값이 여기 나옵니다!"
+
+
+}
+
+
 pickRandomNum();
